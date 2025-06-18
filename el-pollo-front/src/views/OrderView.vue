a3348e4c61efbe5e7a0478734e1a47383208ae1c
Félix Viart <viart.felix@gmail.com>
2025-06-18 15:33:36 +0200

    WIP Order page styles

--- el-pollo-front/src/views/OrderView.vue
+++ el-pollo-front/src/views/OrderView.vue
@@ -1,51 +1,56 @@
 <script setup lang="ts">
 import OrderBanner from '@/components/order/OrderBanner.vue'
-import type { Consumable } from '@/models/consumable.ts'
+import type { Burger, Drink } from '@/models/consumable.ts'
 import OrderItemCard from '@/components/order/OrderItemCard.vue'
 import { getConsumableTypeForItem } from '@/utils/consumable-utils.ts'
 import { onMounted, ref } from 'vue'
+import { useBasketStore } from '@/stores/basketStore.ts'
 import { getHttpClient } from '@/plugins/http-client.ts'
-import type { ConsumableState } from '@/stores/consumableStore.ts'
+import OrderSummary from '@/components/order/OrderSummary.vue'
 
-const getItems = () => {
+const basketStore = useBasketStore()
+
+const itemsRef = ref< (Drink|Burger)[] >(basketStore.basket as (Drink|Burger)[])
+
+onMounted(() => {
+  const id = Math.round(Math.random() * 27)
+
   getHttpClient()
-    .get('/api/consumables')
+    .get(`/api/burgers/${id}`)
     .then((r) => {
-      const data = r.data as ConsumableState
+      r.data.quantity = Math.floor( Math.random() * 4 ) + 1
 
-      itemsRef.value = data.burgers
+      basketStore.addItem(r.data)
+
+      console.log(basketStore.basket)
     })
     .catch((e) => {
       console.log(e)
     })
-}
-
-const itemsRef = ref<Consumable[]|null>(null)
-
-onMounted(() => {
-  getItems()
 })
-
 </script>
 
 <template>
-  <section v-if="itemsRef">
-    <OrderBanner :items="itemsRef as Consumable[]" />
+  <OrderBanner :items="basketStore.basket" />
 
-    <div style="display: grid; grid-template-columns: repeat(4, 1fr); gap: 2rem">
-      <OrderItemCard
-        v-for="(item, key) in itemsRef"
-        :item="item"
-        :type="getConsumableTypeForItem(item)"
-        :key="key"
-      />
-    </div>
-  </section>
+  <div style="display: grid; grid-template-columns: repeat(4, 1fr); gap: 2rem">
+    <OrderItemCard
+      v-for="(item, key) in itemsRef"
+      :item="item"
+      :type="getConsumableTypeForItem(item)"
+      :key="key"
+    />
+  </div>
 
-  <section v-else>
-    <v-progress-circular />
-    <h1>Chargement...</h1>
-  </section>
+  <v-divider></v-divider>
+
+  <!-- container for the right-resume -->
+  <div class="position-fixed right-0 top-0 w-full">
+    <OrderSummary
+      :total="basketStore.getTotal(true) as number"
+      :items="itemsRef"
+    />
+  </div>
 </template>
 
 <style scoped lang="ts">
